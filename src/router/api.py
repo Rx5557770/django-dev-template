674e37467c03f76c1d5303b9cf711effae336ce4
Store <onlynnn0@gmail.com>
2026-07-12 16:27:06 +0800

    添加了私密接口和公开接口

--- src/router/api.py
+++ src/router/api.py
@@ -1,15 +1,23 @@
 # src/router/api.py
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Router
 from decouple import config
 
-from src.user.api import api as user_api
+from src.auth.auth import AdminBearer
+from src.user.api import private_api as user_private_api, public_api as user_public_api
 
 api = NinjaAPI()
+admin_api = Router()
+api.add_router("admin", admin_api, auth=AdminBearer())
+
 # 验证码
 if config("CAPTCHA", cast=bool):
     from src.utils.captcha import api as captcha_api
 
     api.add_router("captcha", captcha_api, tags=["Captcha"])
 
+### 管理员接口 ###
+admin_api.add_router("users", user_private_api, tags=["Private-Users"])
 
-api.add_router("users", user_api, tags=["Users"])
+
+### 公开接口 ###
+api.add_router("users", user_public_api, tags=["Public-Users"])
--- src/user/api.py
+++ src/user/api.py
@@ -22,48 +22,48 @@ from src.utils.jwt import create_token
 
 User = get_user_model()
 
-api = Router()
-# auth_api = Router()
+private_api = Router()
+public_api = Router()
 
 
 ### 普通用户 ###
-# @auth_api.post("login", response={200: TokenSchema, 400: dict})
-# def login(request, payload: LoginSchema):
-#     user = get_object_or_404(User, username=payload.username)
-#     if not user.check_password(payload.password):
-#         raise HttpError(400, "账号或密码错误")
-#     token = create_token(user)
-#     return {"access_token": token}
+@public_api.post("login", response={200: TokenSchema, 400: dict})
+def login(request, payload: LoginSchema):
+    user = get_object_or_404(User, username=payload.username)
+    if not user.check_password(payload.password):
+        raise HttpError(400, "账号或密码错误")
+    token = create_token(user)
+    return {"access_token": token}
 
 
-# @auth_api.post("register")
-# def register(request, payload: RegisterSchema):
-#     if (
-#         User.objects.filter(username=payload.username).exists()
-#         or User.objects.filter(email=payload.email).exists()
-#     ):
-#         raise HttpError(400, "账号已存在")
-#     user = User.objects.create_user(**payload.model_dump(exclude='password2'))
+@public_api.post("register")
+def register(request, payload: RegisterSchema):
+    if (
+        User.objects.filter(username=payload.username).exists()
+        or User.objects.filter(email=payload.email).exists()
+    ):
+        raise HttpError(400, "账号已存在")
+    user = User.objects.create_user(**payload.model_dump(exclude="password2"))
 
-#     return {"detail": "注册成功"}
+    return {"detail": "注册成功"}
 
 
-# @auth_api.post("reset-password", auth=AuthBearer())
-# def reset_password(request, payload: ResetPasswordSchema):
-#     user = request.auth
-#     if not user.check_password(payload.old_password):
-#         raise HttpError(400, "旧密码错误")
+@public_api.post("reset-password", auth=AuthBearer())
+def reset_password(request, payload: ResetPasswordSchema):
+    user = request.auth
+    if not user.check_password(payload.old_password):
+        raise HttpError(400, "旧密码错误")
 
-#     user.set_password(payload.new_password)
-#     user.save()
-#     return {"detail": "密码修改成功"}
+    user.set_password(payload.new_password)
+    user.save()
+    return {"detail": "密码修改成功"}
 
 
 ### 以下是通用格式 ###
 db_model = User
 
 
-@api.post("/", response=out_model)
+@private_api.post("/", response=out_model)
 def create(request, payload: create_model):
     if (
         User.objects.filter(username=payload.username).exists()
@@ -73,7 +73,7 @@ def create(request, payload: create_model):
     return db_model.objects.create(**payload.model_dump())
 
 
-@api.get("/", response=list[out_model])
+@private_api.get("/", response=list[out_model])
 @paginate(CustomPagination)
 def list_data(request, filters: Query[filter_model]):
     querysets = db_model.objects.all()
@@ -81,12 +81,12 @@ def list_data(request, filters: Query[filter_model]):
     return querysets
 
 
-@api.get("/{id}", response=out_model)
+@private_api.get("/{id}", response=out_model)
 def detail_data(request, id: int):
     return get_object_or_404(db_model, id=id)
 
 
-@api.put("/{id}", response=out_model)
+@private_api.put("/{id}", response=out_model)
 def update_data(request, id: int, payload: update_model):
     data = get_object_or_404(db_model, id=id)
 
@@ -97,7 +97,7 @@ def update_data(request, id: int, payload: update_model):
     return data
 
 
-@api.delete("/{id}")
+@private_api.delete("/{id}")
 def delete_data(request, id: int):
     data = get_object_or_404(db_model, id=id)
     data.delete()
